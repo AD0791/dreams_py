64790ad80ccf40d80c466a741b2deb0415a980d5
AD0791 <alexdisla1991@gmail.com>
2021-06-02 15:27:22 -0400

    Datim is ready

---
+++ agyw_prev_datim/agyw.py
@@ -0,0 +1,135 @@
+from layeringOfServices import DREAMS_MASTERSHEET
+
+class AgywPrev:
+    """A class with properties and methods given the results of the indicator AGYW_PREV DATIM""" 
+    __who_am_I ="DATIM"
+    __datim1_title="Number of individual AGYW that have fully completed the entire DREAMS primary package of services but have not received any services beyond the primary package."
+    __datim2_title="Number of individual AGYW that have fully completed the entire DREAMS primary package of services AND at least one additional secondary service."
+    __datim3_title="Number of individual AGYW that have fully completed at least one DREAMS primary package of services but not the full primary package of services."
+    
+    
+    @classmethod
+    def datim_titleI(cls):
+        return cls.__datim1_title
+    
+    @classmethod
+    def datim_titleII(cls):
+        return cls.__datim2_title
+    
+    @classmethod
+    def datim_titleIII(cls):
+        return cls.__datim3_title
+    
+    
+    def __init__(self,commune=None):
+        self.__commune = commune
+        self.__i_am = f"{AgywPrev.__who_am_I}"
+        self.__data =  DREAMS_MASTERSHEET
+        self. __total_mastersheet = self.__data.code.count()
+        if self.__commune == None:
+            self.__dreams_valid = self.__data[(self.__data.age_range !="not_valid_age") & (self.__data.age_range != "25-29") & (self.__data.timeOn_system == "required_Time_on")]
+        else:
+            self.__dreams_valid = self.__data[(self.__data.age_range !="not_valid_age") & (self.__data.age_range != "25-29") & (self.__data.timeOn_system == "required_Time_on")&(self.__data.actual_commune == f"{self.__commune}")]
+        self.__total_dreams_valid = self.__dreams_valid.code.count()
+        self.__dreams_valid["primary_only"] = self.__dreams_valid.apply(lambda df: self.__primFunc(df),axis=1)
+        self.__dreams_valid["primary_and_OneSecondary_services"] = self.__dreams_valid.apply(lambda df: self.__primLeastOneSecFunc(df),axis=1)
+        self.__dreams_valid["partial_primary_services"] = self.__dreams_valid.apply(lambda df: self.__primPartFunc(df),axis=1)
+        self.__agyw_prevI = self.__dreams_valid[self.__dreams_valid.primary_only == "full_primary_only"]
+        self.__agyw_prevII = self.__dreams_valid[self.__dreams_valid.primary_and_OneSecondary_services == "full_primary_leastOneSecondary"]
+        self.__agyw_prevIII = self.__dreams_valid[self.__dreams_valid.partial_primary_services == "primary_part_services"]  
+        self.__agyw_prevI_total = self.__agyw_prevI.code.count()
+        self.__agyw_prevII_total = self.__agyw_prevII.code.count()
+        self.__agyw_prevIII_total = self.__agyw_prevIII.code.count()
+        self.__total_datim = self.__agyw_prevI_total + self.__agyw_prevII_total + self.__agyw_prevIII_total
+        
+        
+    @property
+    def who_am_i(self):
+        return self.__i_am
+    
+    
+    @property
+    def total_mastersheet(self):
+        return self.__total_mastersheet
+
+    @property
+    def total_dreams_valid(self):
+        return self.__total_dreams_valid
+    
+    def __primFunc(self,df):
+        if (df.ps_10_14 == "curriculum-servis" and df.hts == "errata" and df.condoms == "errata" and df.post_care_treatment == "no" and df.socio_eco_app == "no" ):
+            return "full_primary_only"
+        elif (df.ps_15_19 == "condoms&curriculum" and df.hts == "errata"  and df.post_care_treatment == "no" and df.socio_eco_app == "no" ):
+            return "full_primary_only"
+        elif (df.ps_20_24 == "condoms&hts&curriculum" and df.post_care_treatment == "no" and df.socio_eco_app == "no" ):
+            return "full_primary_only"
+        else:
+            return "invalid"
+    
+    def __primLeastOneSecFunc(self,df):
+        if (df.ps_10_14 == "curriculum-servis") and (df.hts == "tested_on_given_date" or df.condoms == "tested_on_given_date" or df.post_care_treatment == "service_gyneco_vbg" or df.socio_eco_app == "service_muso_gardening" ):
+            return "full_primary_leastOneSecondary"
+        elif (df.ps_15_19 == "condoms&curriculum") and (df.hts == "tested_on_given_date" or df.post_care_treatment == "service_gyneco_vbg" or df.socio_eco_app == "service_muso_gardening" ):
+            return "full_primary_leastOneSecondary"
+        elif (df.ps_20_24 == "condoms&hts&curriculum") and (df.post_care_treatment == "service_gyneco_vbg" or df.socio_eco_app == "service_muso_gardening" ):
+            return "full_primary_leastOneSecondary"
+        else:
+            return "invalid"
+        
+    def __primPartFunc(self,df):
+        if (df.age_range == "15-19") and ((df.condoms == "tested_on_given_date" and df.dreams_curriculum == "curriculum_inc") or (df.condoms == "errata" and df.dreams_curriculum == "curriculum_completed")):
+            return "primary_part_services"
+        elif (df.age_range == "20-24") and ((df.dreams_curriculum == "curriculum_completed" and df.condoms=="errata" and df.hts=="errata")or(df.dreams_curriculum == "curriculum_completed" and df.condoms=="tested_on_given_date" and df.hts=="errata")or(df.dreams_curriculum == "curriculum_completed" and df.condoms=="errata" and df.hts=="tested_on_given_date")or(df.dreams_curriculum == "curriculum_inc" and df.condoms=="tested_on_given_date" and df.hts=="errata")or(df.dreams_curriculum == "curriculum_inc" and df.condoms=="errata" and df.hts=="tested_on_given_date")or(df.dreams_curriculum == "curriculum_inc" and df.condoms=="tested_on_given_date" and df.hts=="tested_on_given_date")):
+            return "primary_part_services"
+        else:
+            return "invalid"
+         
+    @property
+    def total_datimI(self):
+        return self.__agyw_prevI_total
+    
+    @property
+    def total_datimII(self):
+        return self.__agyw_prevII_total
+    
+    @property
+    def total_datimIII(self):
+        return self.__agyw_prevIII_total
+    
+    @property
+    def total_datim_general(self):
+        return self.__total_datim
+
+
+    def datim_agyw_prevI(self):
+        agyw_prevI_pivot = self.__agyw_prevI.pivot_table(index=["age_range"],columns=["agyw_period_range"], values="code",aggfunc="count",fill_value=0,margins=True,margins_name="Total")
+        agyw_prevI_pivot.rename_axis(index={"age_range":"Age"},columns={"agyw_period_range":"Time"},inplace=True)
+        agyw_prevI_pivot_final = agyw_prevI_pivot[:-1]
+        return agyw_prevI_pivot_final
+    
+    def datim_agyw_prevII(self):
+        agyw_prevII_pivot = self.__agyw_prevII.pivot_table(index=["age_range"],columns=["agyw_period_range"], values="code",aggfunc="count",fill_value=0,margins=True,margins_name="Total")
+        agyw_prevII_pivot.rename_axis(index={"age_range":"Age"},columns={"agyw_period_range":"Time"},inplace=True)
+        agyw_prevII_pivot_final = agyw_prevII_pivot[:-1]
+        return agyw_prevII_pivot_final
+    
+    def datim_agyw_prevIII(self):
+        agyw_prevIII_pivot = self.__agyw_prevIII.pivot_table(index=["age_range"],columns=["agyw_period_range"], values="code",aggfunc="count",fill_value=0,margins=True,margins_name="Total")
+        agyw_prevIII_pivot.rename_axis(index={"age_range":"Age"},columns={"agyw_period_range":"Time"},inplace=True)
+        agyw_prevIII_pivot_final = agyw_prevIII_pivot[:-1]
+        return agyw_prevIII_pivot_final
+
+
+
+class AgywPrevCommune(AgywPrev):
+    """A class that extend AgywPrev with the purpose of the indicator AGYW_PREV DATIM by commune"""
+    __who_am_I ="DATIM"
+    def __init__(self,name):
+        self.__name = name
+        self.__i_am = f"{AgywPrevCommune.__who_am_I} {self.__name}"
+        #self.__data =  DREAMS_MASTERSHEET
+        super().__init__(self.__name)
+        
+    @property
+    def who_am_i(self):
+        return self.__i_am
