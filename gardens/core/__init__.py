45c4fbeb6276b4cdba5deca58c773d25d89fe1d5
AD0791 <alexdisla1991@gmail.com>
2022-07-12 12:46:21 -0400

    get ready to succeed

--- gardens/core/__init__.py
+++ gardens/core/__init__.py
@@ -1,1 +1,11 @@
+from .agyw import AgywPrev
+
+
+_datim = AgywPrev()
+BASE = _datim.data_dreams_valid
+GARDENING_SERVED = BASE[
+    BASE.gardening == "yes"
+]
+
+
 WHO_AM_I = 'core of version  22 updated of agyw datim script'
--- gardens/core/active.py
+++ gardens/core/active.py
@@ -7,7 +7,7 @@ import pandas as pd
 from numpy import int16
 from enum import Enum
 
-from functions import *
+from .functions import *
 
 load_dotenv()
 
@@ -20,9 +20,9 @@ DBNAME = config('DBCaris')
 
 class Set_date(Enum):
     master_start = "2017-10-01"
-    master_end = "2022-07-06"
+    master_end = "2022-07-12"
     period_start = "2021-10-01"
-    period_end = "2022-07-06"
+    period_end = "2022-07-12"
 
 
 # get the engine to connect and fetch
